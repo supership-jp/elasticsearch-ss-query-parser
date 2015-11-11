4d0aec71993c3cae134ed98762fb19cd3e8eeff1
大川真吾 <shingo.okawa@galleon.local>
2015-11-11 12:21:23 +0900

    implement range-term parsing functionality.

--- src/main/java/jp/supership/elasticsearch/plugin/queryparser/classic/intermediate/QueryParser.java
+++ src/main/java/jp/supership/elasticsearch/plugin/queryparser/classic/intermediate/QueryParser.java
@@ -3,7 +3,11 @@
  */
 package jp.supership.elasticsearch.plugin.queryparser.classic.intermediate;
 
+import java.io.IOException;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 // TODO: Check which CharStream is called.
 // TODO: Check if the FastCharStream must be implemented or not.
@@ -19,6 +23,7 @@ import org.apache.lucene.search.DisjunctionMaxQuery;
 import org.apache.lucene.search.MultiPhraseQuery;
 import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermRangeQuery;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser.Operator;
 import org.apache.lucene.queryparser.classic.TokenMgrError;
@@ -173,7 +178,11 @@ public abstract class QueryParser extends QueryBuilder implements QueryHandler {
     }
 
     /**
-     * Returns 
+     * Returns {@code FieldQuery} in accordance to the assigned configuration.
+     * @param  field the currently handling field.
+     * @param  queryText the currently handling raw query string.
+     * @param  quoted this value must be ser true if the handling query is considered to be quoted.
+     * @return the resulting {@code Query} instance.
      * @throws ParseException if the parsing fails.
      */
     protected Query getFieldQuery(String field, String queryText, boolean quoted) throws ParseException {
@@ -181,7 +190,30 @@ public abstract class QueryParser extends QueryBuilder implements QueryHandler {
     }
 
     /**
-     *
+     * Base implementation delegates to {@link #getFieldQuery(String, String, boolean)}.
+     * This method may be overridden, for example, to return a SpanNearQuery instead of a PhraseQuery.
+     * @exception org.apache.lucene.queryparser.classic.ParseException throw in overridden method to disallow
+     */
+    protected Query getFieldQuery(String field, String queryText, int phraseSlop) throws ParseException {
+        Query query = this.getFieldQuery(field, queryText, true);
+
+        if (query instanceof PhraseQuery) {
+            ((PhraseQuery) query).setSlop(phraseSlop);
+        }
+        if (query instanceof MultiPhraseQuery) {
+            ((MultiPhraseQuery) query).setSlop(phrasSlop);
+        }
+
+        return query;
+    }
+
+    /**
+     * Returns {@code FieldQuery} in accordance to the assigned configuration.
+     * @param  analyzer the analyzer instance which is responsible for the raw query analysis.
+     * @param  field the currently handling field.
+     * @param  queryText the currently handling raw query string.
+     * @param  quoted this value must be ser true if the handling query is considered to be quoted.
+     * @return the resulting {@code Query} instance.
      * @throws ParseException if the parsing fails.
      */
     protected Query newFieldQuery(Analyzer analyzer, String field, String queryText, boolean quoted) throws ParseException {
@@ -205,102 +237,29 @@ public abstract class QueryParser extends QueryBuilder implements QueryHandler {
                 return null;
             }
             return this.getBooleanQuery(clauses, true);
-        } else if (quoted == false) {
-        }
+        // TODO: THIS MUST BE HANDLED WITHIN {@code TokenFileter} WHICH IMPLEMENTS NAIVE BAYSIAN FILTER.
+        } else if (quoted == false && queryText.matches("^\\d+(\\.\\d+)?.{1,2}?$")) {
+	    quoted = true;
+	    this.queryParsingContext.setPhraseSlop(0);
+        } else {
+	    quoted = quoted || this.queryParsingContext.getPhraseQueryAutoGeneration();
+	}
+
+	return this.createFieldQuery(analyzer, occurence, field, queryText, quoted, this.queryParsingContext.getPhraseSlop());
     }
 
     /**
-     * INTERNAL USE ONLY.
-     * This class represents internal field-query-parsing context.
-     */
-    private class FieldQueryParsingContext {
-        // Holds CachingTokenFilter for enabling lookup functionality.
-        public CachingTokenFilter buffer = null;
-        // Holds byte-data refference.
-        public TermToBytesRefAttribute termToBytesRef = null;
-        // Holds relative position.
-        public PositionIncrementAttribute positionIncrement = null;
-        // Holds number of tokens.
-        public int numberOfTokens = 0;
-        // Holds position count.
-        public int positionCount = 0;
-        // true if several tokens appears at the same position.
-        public boolean severalTokensAtSamePosition = false;
-        // true if buffer has more tokens.
-        public boolean hasMoreTokens = false;
-
-        // Constructor.
-        public FieldQueryParsingContext(TokenStream tokenStream, boolean reset) {
-            if (reset) {
-                tokenStream.reset();
-            }
-            this.buffer = new CachingTokenFilter(tokenStream);
-            this.buffer.reset();
-            this.termToBytesRef = this.buffer.getAttribute(TermToBytesRefAttribute.class);
-            this.positionIncrement = this.buffer.getAttribute(PositionIncrementAttribute.class);
-            this.prepare();
-        }
-
-        // Constructor.
-        public FieldQueryParsingContext(TokenStream tokenStream) {
-            this(tokenStream, true);
-        }
-
-        // Prepares for parsing.
-        private void prepare() {
-            if (this.termToBytesRef != null) {
-                try {
-                    this.hasMoreTokens = this.incrementToken();
-                    while (this.hasMoreTokens) {
-                        this.numberOfTokens++;
-                        int increment = (this.positionIncrement != null) ? this.getPositionIncrement() : 1;
-                        if (increment != 0) {
-                            this.positionCount += increment;
-                        } else {
-                            this.severalTokensAtSamePosition = true;
-                        }
-                        this.hasMoreTokens = this.buffer.incrementToken();
-                    }
-                } catch (IOException e) {
-                    // DO NOTHING.
-                }
-            }
-            this.buffer.reset();
-        }
-
-        // Delegates {@code CachingTokenFilter}'s method.
-        public boolean incrementToken() throws IOException {
-            return this.buffer.incrementToken();
-        }
-
-        // Delegates {@code PositionIncrementAttribute}'s method.
-        public int getPositionIncrement() {
-            return this.positionIncrement == null ? 0 : this.positionIncrement.getPositionIncrement();
-        }
-
-        // Delegates {@code PositionIncrementAttribute}'s method.
-        public BytesRef getBytesRef() {
-            return this.termToBytesRef == null ? null : this.termToBytesRef.getBytesRef();
-        }
-
-        // Delegates {@code PositionIncrementAttribute}'s method.
-        public void fillBytesRef() {
-            this.termToBytesRef.fillBytesRef();
-        }
-    }
-
-    /**
-     *
+     * @inheritDoc
      */
     @Override
     protected Query createFieldQuery(Analyzer analyzer, BooleanClause.Occur operator, String field, String queryText, boolean quoted, int phraseSlop) {
         assert operator == BooleanClause.Occur.SHOULD || operator == BooleanClause.Occur.MUST;
         TokenStream source = null;
-        FieldQueryParsingContext context = null;
+        TokenStreamParsingContext context = null;
 
         try {
             source = analyzer.tokenStream(field, queryText);
-            context = new FieldQueryParsingContext(source);
+            context = new TokenStreamParsingContext(source);
         } catch (IOException cause) {
             throw new RuntimeException("error analyzing query text", cause);
         } finally {
@@ -408,7 +367,7 @@ public abstract class QueryParser extends QueryBuilder implements QueryHandler {
                 query.setSlop(phraseSlop);
                 int position = -1;
               
-                for (int i = 0; i < numTokens; i++) {
+                for (int i = 0; i < context.numberOfTokens; i++) {
                     int increment = 1;
                   
                     try {
@@ -431,6 +390,218 @@ public abstract class QueryParser extends QueryBuilder implements QueryHandler {
                 }
                 return query;
             }
+        }
+    }
+
+    /**
+     * Returns {@code TermRangeQuery} in accordance to the assigned configuration.
+     * @param  field the currently handling field.
+     * @param  infinimum the assigned range's infinimum.
+     * @param  supremum the assigned range's supremum.
+     * @param  leftInclusive true if the infinimum of the range is inclusive
+     * @param  rightInclusive true if the supremum of the range is inclusive
+     * @return new {@link TermRangeQuery} instance
+     */
+    protected Query getRangeQuery(String field, String infinimum, String supremum, boolean leftInclusive, boolean rightInclusive) throws ParseException {
+        if (this.queryParsingContext.getLowercaseExpandedTerms()) {
+            infinimum = infinimum == null ? null : infinimum.toLowerCase(this.queryParsingContext.getLocale());
+            supremum = supremum == null ? null : supremum.toLowerCase(this.queryParsingContext.getLocale());
+        }
+
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale);
+        dateFormat.setLenient(true);
+        DateTools.Resolution resolution = this.queryParsingContext.getDateResolution(field);
+
+        try {
+            infinimum = DateTools.dateToString(dateFormat.parse(infinimum), resolution);
+        } catch (Exception e) {
+	    // DO NOTHING, we know the boundaries are filled with somewhat instances.
+	}
+
+        try {
+            Date until = dateFormat.parse(supremum);
+            if (rightInclusive) {
+                // The user can only specify the date, not the time, so make sure
+                // the time is set to the latest possible time of that date to really
+                // include all documents:
+                Calendar calendar = Calendar.getInstance(this.queryParsingContext.getTimeZone(), this.queryParsingContext.getLocale());
+                calendar.setTime(until);
+                calendar.set(Calendar.HOUR_OF_DAY, 23);
+                calendar.set(Calendar.MINUTE, 59);
+                calendar.set(Calendar.SECOND, 59);
+                calendar.set(Calendar.MILLISECOND, 999);
+                until = calendar.getTime();
+            }
+            supremum = DateTools.dateToString(until, resolution);
+        } catch (Exception e) {
+	    // DO NOTHING, we know the boundaries are filled with somewhat instances.
+	}
+
+        return this.newRangeQuery(field, infinimum, supremum, leftInclusive, rightInclusive);
+    }
+
+    /**
+     * Returns {@code TermRangeQuery} in accordance to the assigned configuration.
+     * @param  field the currently handling field.
+     * @param  infinimum the assigned range's infinimum.
+     * @param  supremum the assigned range's supremum.
+     * @param  leftInclusive true if the infinimum of the range is inclusive
+     * @param  rightInclusive true if the supremum of the range is inclusive
+     * @return new {@link TermRangeQuery} instance
+     */
+    protected Query newRangeQuery(String field, String infinimum, String supremum, boolean leftInclusive, boolean rightInclusive) {
+        final BytesRef left;
+        final BytesRef right;
+     
+        if (infinimum == null) {
+            left = null;
+        } else {
+            left = this.queryParsingContext.getRangeTermAnalysis() ? this.analyzeAsSingleMultiTerm(field, infinimum) : new BytesRef(infinimum);
+        }
+     
+        if (supremum == null) {
+            right = null;
+        } else {
+            right = this.queryParsingContext.getRangeTermAnalysis() ? this.analyzeAsSingleMultiTerm(field, supremum) : new BytesRef(supremum);
+        }
+      
+        final TermRangeQuery query = new TermRangeQuery(field, left, right, leftInclusive, rightInclusive);
+        query.setRewriteMethod(this.queryParsingContext.getMultiTermRewriteMethod());
+        return query;
+    }
+
+    /**
+     * INTERNAL USE ONLY.
+     * This class represents internal field-query-parsing context.
+     */
+    protected class TokenStreamParsingContext {
+        // Holds CachingTokenFilter for enabling lookup functionality.
+        public CachingTokenFilter buffer = null;
+        // Holds byte-data refference.
+        public TermToBytesRefAttribute termToBytesRef = null;
+        // Holds relative position.
+        public PositionIncrementAttribute positionIncrement = null;
+        // Holds number of tokens.
+        public int numberOfTokens = 0;
+        // Holds position count.
+        public int positionCount = 0;
+        // true if several tokens appears at the same position.
+        public boolean severalTokensAtSamePosition = false;
+        // true if buffer has more tokens.
+        public boolean hasMoreTokens = false;
+
+        // Constructor.
+        public TokenStreamParsingContext(TokenStream tokenStream, boolean reset) {
+            if (reset) {
+                tokenStream.reset();
+            }
+            this.buffer = new CachingTokenFilter(tokenStream);
+            this.buffer.reset();
+            this.termToBytesRef = this.buffer.getAttribute(TermToBytesRefAttribute.class);
+            this.positionIncrement = this.buffer.getAttribute(PositionIncrementAttribute.class);
+            this.prepare();
+        }
+
+        // Constructor.
+        public TokenStreamParsingContext(TokenStream tokenStream) {
+            this(tokenStream, true);
+        }
+
+        // Prepares for parsing.
+        private void prepare() {
+            if (this.termToBytesRef != null) {
+                try {
+                    this.hasMoreTokens = this.incrementToken();
+                    while (this.hasMoreTokens) {
+                        this.numberOfTokens++;
+                        int increment = (this.positionIncrement != null) ? this.getPositionIncrement() : 1;
+                        if (increment != 0) {
+                            this.positionCount += increment;
+                        } else {
+                            this.severalTokensAtSamePosition = true;
+                        }
+                        this.hasMoreTokens = this.buffer.incrementToken();
+                    }
+                } catch (IOException e) {
+                    // DO NOTHING.
+                }
+            }
+            this.buffer.reset();
+        }
+
+	// Delegates {@code TokenStream}'s method.
+	public void tokenStreamReset() throws IOException {
+	    this.tokenStream.reset();
+	}
+
+	// Delegates {@code TokenStream}'s method.
+	public void tokenStreamEnd() throws IOException {
+	    this.tokenStream.end();
+	}
+
+        // Delegates {@code CachingTokenFilter}'s method.
+        public boolean incrementToken() throws IOException {
+            return this.buffer.incrementToken();
+        }
+
+        // Delegates {@code PositionIncrementAttribute}'s method.
+        public int getPositionIncrement() {
+            return this.positionIncrement == null ? 0 : this.positionIncrement.getPositionIncrement();
+        }
+
+        // Delegates {@code PositionIncrementAttribute}'s method.
+        public BytesRef getBytesRef() {
+            return this.termToBytesRef == null ? null : this.termToBytesRef.getBytesRef();
+        }
+
+        // Delegates {@code PositionIncrementAttribute}'s method.
+        public void fillBytesRef() {
+            this.termToBytesRef.fillBytesRef();
+        }
+    }
+
+    /**
+     * Analyzes the given query part as a single {@code MultiTermQuery} in accordance to the assigned configuration.
+     * @param  field the currently handling field.
+     * @param  queryPart the assigned range's infinimum.
+     * @return new {@link BytesRef} instance
+     */
+    protected BytesRef analyzeAsSingleMultiTerm(String field, String queryPart) {
+        return this.analyzeMultitermTerm(field, queryPart, this.getAnalyzer());
+    }
+
+    /**
+     * Analyzes the given query part as a single {@code MultiTermQuery} in accordance to the assigned configuration.
+     * @param  field the currently handling field.
+     * @param  queryPart the assigned range's infinimum.
+     * @param  analyzer the analyzer instance which is responsible for the raw query analysis.
+     * @return new {@link BytesRef} instance
+     */
+    protected BytesRef analyzeAzSingleMultiTerm(String field, String part, Analyzer analyzer) {
+        if (analyzer == null) {
+	    analyzer = this.getAnalyzer();
+	}
+        TokenStream source = null;
+	TokenStreamParsingContext context = null;
+
+        try {
+            source = analyzer.tokenStream(field, part);
+	    context = new TokenStreamParsingContext(source);
+            BytesRef bytes = context.getBytesRef();
+
+            if (!context.incrementToken()) {
+                throw new IllegalArgumentException("analyzer returned no terms for multiTerm term: " + part);
+	    }
+            context.fillBytesRef();
+            if (context.incrementToken()) {
+                throw new IllegalArgumentException("analyzer returned too many terms for multiTerm term: " + part);
+	    }
+            context.tokenStreamEnd();
+            return BytesRef.deepCopyOf(bytes);
+        } catch (IOException cause) {
+            throw new RuntimeException("error analyzing multiTerm term: " + part, cause);
+        } finally {
+            IOUtils.closeWhileHandlingException(source);
         }
     }
 }
